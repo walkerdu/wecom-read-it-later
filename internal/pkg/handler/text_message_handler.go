8cbb04606b872250e3176a6786bbef1211dbd20c
walkerdu <walkerdu@tencent.com>
2024-02-23 17:16:59 +0800

    add /cmd for review

--- internal/pkg/handler/text_message_handler.go
+++ internal/pkg/handler/text_message_handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 	"log"
+	"strconv"
 	"strings"
 	"time"
 
@@ -22,7 +23,7 @@ type TextMessageHandler struct {
 }
 
 var commandsMap = map[string]struct{}{
-	"/day":       struct{}{},
+	"/today":     struct{}{},
 	"/yesterday": struct{}{},
 	"/week":      struct{}{},
 	"/month":     struct{}{},
@@ -36,15 +37,33 @@ func (t *TextMessageHandler) HandleMessage(msg wecom.MessageIF) (wecom.MessageIF
 	textMsg := msg.(*wecom.TextMessageReq)
 
 	var chatRsp string
+	var err error
+	content := strings.TrimSpace(textMsg.Content)
 
-	if !strings.HasPrefix(textMsg.Content, "/") {
-		_, err := t.DBSet(textMsg)
-		if err != nil {
-			chatRsp = err.Error()
-		} else {
+	for {
+		if !strings.HasPrefix(content, "/") {
+			err = t.DBSet(textMsg)
+			if err != nil {
+				break
+			}
 			chatRsp = "success"
+			break
 		}
-	} else {
+
+		switch content {
+		case "/today":
+			chatRsp, err = t.SummaryTodDay(textMsg)
+		case "/month":
+			chatRsp, err = t.SummaryMonth(textMsg)
+		default:
+			chatRsp, err = t.SummaryDay(textMsg)
+		}
+
+		break
+	}
+
+	if err != nil {
+		chatRsp = err.Error()
 	}
 
 	//chatRsp, err := chatbot.MustChatbot().GetResponse(textMsg.FromUserName, textMsg.Content)
@@ -60,16 +79,98 @@ func (t *TextMessageHandler) HandleMessage(msg wecom.MessageIF) (wecom.MessageIF
 	return &textMsgRsp, nil
 }
 
-func (t *TextMessageHandler) DBSet(msg *wecom.TextMessageReq) (*wecom.TextMessageRsp, error) {
+func (t *TextMessageHandler) DBSet(msg *wecom.TextMessageReq) error {
 	ctx := context.Background()
 	date := time.Now().Format("20060102")
 	key := msg.FromUserName + "_" + date
-	result, err := HandlerInst().redisClient.LPush(ctx, key, msg.Content).Result()
+	result, err := HandlerInst().redisClient.RPush(ctx, key, msg.Content).Result()
 	if err != nil {
 		log.Printf("[ERROR][DBSet] redis LPush failed, err=%s", err)
-		return nil, err
+		return err
 	}
 
 	log.Printf("[DEBUG][DBSet] redis LPush success, key:%v, value:%v, result=%v", key, msg.Content, result)
-	return nil, nil
+	return nil
+}
+
+func (t *TextMessageHandler) SummaryBase(ctx context.Context, key string) (string, error) {
+	vals, err := HandlerInst().redisClient.LRange(ctx, key, 0, -1).Result()
+	if err != nil {
+		log.Printf("[ERROR][SummaryBase] redis LRange failed, err=%s", err)
+		return "", err
+	}
+
+	summary := ""
+	for idx, val := range vals {
+		summary += strconv.Itoa(idx) + ". " + val + "\n"
+	}
+
+	log.Printf("[DEBUG][SummaryBase] redis LRange success, key:%v, value:%v", key, vals)
+	return summary, nil
+}
+
+func (t *TextMessageHandler) SummaryBaseBatch(ctx context.Context, keyPrefix string) (string, error) {
+	keys, err := HandlerInst().redisClient.Keys(ctx, keyPrefix+"*").Result()
+	if err != nil {
+		log.Printf("[ERROR][SummaryMonth] redis Keys failed, err=%s", err)
+		return "", err
+	}
+
+	summarys := ""
+	for _, key := range keys {
+		summary, err := t.SummaryBase(ctx, key)
+		if err != nil {
+			return "", err
+		}
+
+		summarys += key + "\n" + summary + "\n"
+	}
+
+	return summarys, nil
+}
+
+func (t *TextMessageHandler) SummaryTodDay(msg *wecom.TextMessageReq) (string, error) {
+	ctx := context.Background()
+	date := time.Now().Format("20060102")
+	key := msg.FromUserName + "_" + date
+
+	return t.SummaryBase(ctx, key)
+}
+
+func (t *TextMessageHandler) SummaryYesterday(msg *wecom.TextMessageReq) (string, error) {
+	ctx := context.Background()
+
+	date := time.Now().AddDate(0, 0, -1).Format("20060102")
+	key := msg.FromUserName + "_" + date
+
+	return t.SummaryBase(ctx, key)
+}
+
+func (t *TextMessageHandler) SummaryDay(msg *wecom.TextMessageReq) (string, error) {
+	ctx := context.Background()
+
+	content := strings.TrimSpace(msg.Content)
+	key, found := strings.CutPrefix(content, "/")
+	if !found {
+		return "", nil
+	}
+
+	return t.SummaryBase(ctx, key)
+}
+
+// TODO 本周的前缀不一定相同
+func (t *TextMessageHandler) SummaryWeek(msg *wecom.TextMessageReq) (string, error) {
+	ctx := context.Background()
+	date := time.Now().Format("200601")
+	keyPrefix := msg.FromUserName + "_" + date
+
+	return t.SummaryBaseBatch(ctx, keyPrefix)
+}
+
+func (t *TextMessageHandler) SummaryMonth(msg *wecom.TextMessageReq) (string, error) {
+	ctx := context.Background()
+	date := time.Now().Format("200601")
+	keyPrefix := msg.FromUserName + "_" + date
+
+	return t.SummaryBaseBatch(ctx, keyPrefix)
 }
