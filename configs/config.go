09cd6756a902bdf5aa6350ac227053f1c991703c
walkerdu <walkerdu@tencent.com>
2024-02-23 14:31:38 +0800

    add wecom-read-it-later secrets

--- configs/config.go
+++ configs/config.go
@@ -5,16 +5,10 @@ import (
 	"github.com/walkerdu/wecom-backend/pkg/wecom"
 )
 
-type NotifyConfig struct {
-	UserID string `json:"user_id"`
-	// 群ID
-}
-
 // 企业微信配置
 type WeComConfig struct {
-	AgentConfig  wecom.AgentConfig       `json:"agent_config"`
-	Addr         string                  `json:"addr"`
-	NotifyConfig map[string]NotifyConfig `json:"notify_config,omitempty"`
+	AgentConfig wecom.AgentConfig `json:"agent_config"`
+	Addr        string            `json:"addr"`
 }
 
 type RedisConfig struct {
